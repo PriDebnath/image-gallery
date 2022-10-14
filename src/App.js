f585803da0771e0872f9a43129fa48fad3b062e4
pritam <pritam.debnath@grorapid.com>
2022-10-14 23:11:01 +0530

    version 2

--- src/App.js
+++ src/App.js
@@ -1,76 +1,138 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import "./dist/output.css"
 import KEYS from "./keys";
 import { TextField } from "@mui/material";
 import { Input } from "@mui/material";
 import { Button } from "@mui/material";
 import { ImageList } from "@mui/material";
 import { ImageListItem } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const [searchValue, setSearchValue] = useState("office");
   const [images, setImages] = useState([]);
-  console.log(process.env.REACT_APP_AccessKey);
+  const [page, setPage] = useState(1);
 
-  const getImages = () => {
-    const API_URL = `https://api.unsplash.com/search/photos?page=1&query=${searchValue}&client_id=${KEYS.AccessKey}`;
+  const getImages = (defaultPage) => {
+
+    const API_URL = `https://api.unsplash.com/search/photos?page=${
+      defaultPage ? defaultPage : page
+    }&per_page=30&query=${searchValue}&client_id=${KEYS.AccessKey}`;
+    
+    let toastId;
 
     fetch(API_URL)
       .then((response) => {
-        console.log(response);
+        toastId = toast.loading("Getting images");
         return response.json();
       })
       .then((data) => {
         console.log(data);
-        setImages(data.results);
+        toast.dismiss(toastId);
+        toast.success("succeed");
+        if (defaultPage === 1) {
+          setImages(data.results);
+          setPage(page + 1);
+        } else {
+          setImages([...images, ...data.results]);
+          setPage(page + 1);
+        }
+        console.log(data)
       })
       .catch((error) => {
+        toast.error("Network error");
         console.log(error);
       });
-    console.log("getting images", KEYS.AccessKey);
   };
 
   useEffect(() => {
-    getImages()
+     getImages()
   }, []);
   return (
-    <div>
-      <div className="input_container">
-        <Input
-          type="text"
-          variant='filled'
-          sx={{textAlign:"center"}}
-          InputProps={{
-            startAdornment: <SearchIcon/>,
+    <>
+      <div className="flex flex-col justify-items-center items-center border-2 border-[#1976d2] m-auto p-2 w-[520px] h-screen">
+        <form
+          className=" text-center "
+          onSubmit={(e) => {
+            e.preventDefault();
+            getImages(1);
           }}
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-        />
-        <Button onClick={getImages} variant='outlined'>Get images</Button>
-      </div>
-
-      <div className="p-8 bg-slate-500 input_container">
-        <ImageList
-          sx={{ width: 600, height: 450 }}
-          variant="woven"
-          cols={3}
-          gap={8}
         >
-          {images.map((item, i) => (
-            <ImageListItem key={i}>
-              <img
-                src={`${item.urls.full}?w=164&h=164&fit=crop&auto=format`}
-                srcSet={`${item.urls.full}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                alt={item.alt_description}
-                loading="lazy"
+          <Input
+            type="text"
+            variant="filled"
+            sx={{
+              textAlign: "center",
+              border: 2,
+              borderBottom: "none",
+              borderColor: "#1976d2",
+              paddingLeft: 2,
+              paddingRight: 2,
+              borderRadius: 1,
+              color: "#1976d2",
+            }}
+            
+            endAdornment={
+              <SearchIcon
+                sx={{
+                  cursor: "pointer",
+                  color: "#1976d2",
+                  textAlign: "center",
+                }}
+                onClick={() => {
+                  getImages(1);
+                }}
               />
-            </ImageListItem>
-          ))}
-        </ImageList>
+            }
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </form>
+
+        <div className="p-2 overflow-auto">
+          <ImageList
+            sx={{
+              width: 500,
+              border: 2,
+              borderColor: "#1976d2",
+              borderRadius: 1,
+
+              height: "80vh",
+            }}
+            variant="woven"
+            cols={3}
+            gap={8}
+          >
+            {images &&
+              images.map((item, i) => {
+                return (
+                  <ImageListItem key={i}>
+                    <img
+                      src={`${item.urls.full} `} 
+                      // srcSet={`${item.urls.full}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                      alt={item.alt_description}
+                      loading="lazy"
+                    />
+                  </ImageListItem>
+                );
+              })}
+          </ImageList>
+        </div>
+        <div className="m-auto">
+          <Button
+            variant="outlined"
+            onClick={() => {
+              getImages();
+            }}
+            sx={{ border: 2 }}
+          >
+            More
+          </Button>
+        </div>
       </div>
-    </div>
+      <Toaster />
+    </>
   );
 }
 
