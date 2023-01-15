6da4f0f5cfe91f23ca2e3673a3cd05213c11c248
pritam <debnathpritam0802@gmail.com>
2023-01-15 12:53:01 +0530

    added image download feature

--- src/app/container/gallery/component/NotFound/index.jsx
+++ src/app/container/gallery/component/NotFound/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div> Image not found </div>;
-};
+  return <div className="" > 
+  <h1 style={{width:"100vw",margin:"25vh 0" }} className="text-[#1976d2] text-center">  Image not found . . .
+</h1>
+   </div>;
+}
 
 export default NotFound;
